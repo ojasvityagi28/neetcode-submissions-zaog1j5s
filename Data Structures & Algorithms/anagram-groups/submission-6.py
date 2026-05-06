1e85bc62cf474bfef2ab0aa51ffa730f8afc6049
Ojasvi Tyagi <107610497+ojasvityagi28@users.noreply.github.com>
2026-05-06 16:43:57 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        countmap = {}
+
+        for s in strs:
+            arrcount = [0]*26
+            for char in s:
+                index = ord(char) - ord('a')
+                arrcount[index] +=1
+            if tuple(arrcount) not in countmap:
+                countmap[tuple(arrcount)] = []
+            countmap[tuple(arrcount)].append(s)
+        res = []
+        for key, val in countmap.items():
+            res.append(val)
+        return res
+
+        
