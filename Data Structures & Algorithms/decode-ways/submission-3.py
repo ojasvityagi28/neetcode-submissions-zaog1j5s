e4ffa1fb0ea41a8df3021b1e534ec0df58f2093e
Ojasvi Tyagi <107610497+ojasvityagi28@users.noreply.github.com>
2026-06-18 13:30:33 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if i == len(s):
+                return 1 #found 1 way to decode
+            if s[i] == "0":
+                return 0
+            
+            res = dfs(i + 1)#after taking 1 digit, remaining work is this
+
+            if (i + 1) < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
+                res += dfs(i + 2)
+            dp[i] = res
+            return res
+
+        return dfs(0)
+
+            
+            
+            
+        
