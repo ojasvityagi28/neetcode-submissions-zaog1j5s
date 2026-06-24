aee673ca907ad659eb4058fb039bfcb53d65b801
Ojasvi Tyagi <107610497+ojasvityagi28@users.noreply.github.com>
2026-06-24 19:36:02 +0530

    Add: regular-expression-matching - submission-4

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+
+        def dfs(i , j):
+            if (i , j) in memo:
+                return memo[(i , j)]
+
+            if i >= len(s) and j >= len(p):
+                return True
+            if  j >= len(p):
+                return False     
+
+            memo[(i , j)] = False
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".") 
+
+            if (j + 1) < len(p) and p[j + 1] == "*":
+                memo[(i , j)] = (dfs(i , j + 2)  or 
+                            (match and dfs(i + 1 , j)))
+            elif match:
+                memo[(i , j)] = dfs( i + 1 , j + 1)
+
+            return memo[(i , j)]
+
+        return dfs(0 , 0)
